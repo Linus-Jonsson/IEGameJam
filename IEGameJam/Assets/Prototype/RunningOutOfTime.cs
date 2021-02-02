de42031ccdebe930216c782908cbed589237c545
Jari-van-der-Kolk <58220294+Jari-van-der-Kolk@users.noreply.github.com>
2021-02-02 17:40:19 +0100

    finished prototpye

--- IEGameJam/Assets/Prototype/RunningOutOfTime.cs
+++ IEGameJam/Assets/Prototype/RunningOutOfTime.cs
@@ -1,8 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine; 
+using UnityEngine.UI;
 
 public class RunningOutOfTime : MonoBehaviour
 {
- 
+    
+    private float timer;
+    [SerializeField] private float timerSpeed;
+    private float resetTimer;
+    [SerializeField] private Text timeUI;
+    [SerializeField] private Text gameOverUI;
+
+
+    private void Start() {
+        resetTimer = 100;
+        timer = resetTimer;
+        gameOverUI.gameObject.SetActive(false);
+    }
+
+    private void Update() {
+        timer -= Time.deltaTime * timerSpeed;
+        timeUI.text = string.Format(timer.ToString());
+        if (timer <= 0) {
+            Debug.Log("Game over");
+            gameOverUI.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void ResetTimer() {
+        timer = resetTimer;
+    }
+
 }
--- IEGameJam/Assets/Scripts/Enemy/Spawner.cs
+++ IEGameJam/Assets/Scripts/Enemy/Spawner.cs
@@ -15,26 +15,29 @@ public class Spawner : MonoBehaviour
     [SerializeField] private int amountOfEnemysSpawned;
     Vector3 panda;
 
-
+    
     void Update()
     {
         
         timer += Time.deltaTime * spawnSpeed;
-        if (timer >= 1f) {
+        if (timer >= 1f) 
+        {
             amountOfEnemysSpawned++;
             panda = new Vector3(transform.position.x + Random.Range(0, x),transform.position.y + Random.Range(0, y));
-            Instantiate(PrefabFromArray(prefab), panda,Quaternion.identity);
+            Instantiate(PrefabFromArray(prefab), panda ,Quaternion.identity);
             timer = 0;
         }
         Debug.DrawLine(transform.position, panda);
     }
 
-    private GameObject PrefabFromArray(GameObject[] _prefeb) {
+    private GameObject PrefabFromArray(GameObject[] _prefeb)
+    {
         int index = Random.Range(0, prefab.Length);
         return _prefeb[index];
     }
 
-    private void OnDrawGizmos() {
+    private void OnDrawGizmos() 
+    {
         Gizmos.DrawLine(transform.position,transform.position + Vector3.right * x);    
         Gizmos.DrawLine(transform.position,transform.position + Vector3.up * y);
         Gizmos.DrawWireCube(panda, new Vector3(1, 1, 1));
