3a0d9cfac8ec43a9f2ec9f7f089efa5f3c680b70
Jari-van-der-Kolk <58220294+Jari-van-der-Kolk@users.noreply.github.com>
2021-02-03 14:53:04 +0100

    spawner finished

---
+++ IEGameJam/Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private List<GameObject> spawnPositions;
+    [SerializeField] private float spawnDelay;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        while (true)
+        {
+            Instantiate(prefab, GetSpawnPosition(spawnPositions).transform.position,Quaternion.identity);
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+
+    private GameObject GetSpawnPosition(List<GameObject> _spawnPositions) => _spawnPositions[Random.Range(0, spawnPositions.Count)];
+
+}
+
+
