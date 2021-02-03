bbb97c1e2c88bcfb2f53a459825e26d83fdf3b52
ElmarKotte <Elmo.kotte@gmail.com>
2021-02-03 14:13:55 +0100

    Added zombie animation

--- IEGameJam/Assets/Scripts/Enemy/zombieController.cs
+++ IEGameJam/Assets/Scripts/Enemy/zombieController.cs
@@ -5,18 +5,32 @@ using UnityEngine.AI;
 
 public class zombieController : MonoBehaviour
 {
-    [SerializeField] private Transform plrTransform;
+    [SerializeField] private Transform destination;
     private NavMeshAgent agent;
+    private float distance;
+    private Animator animator;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        agent.SetDestination(plrTransform.position);
+        agent.SetDestination(destination.position);
+        distance = Vector2.Distance(transform.position, destination.position);
+        if (distance <= 2.2f)
+        {
+            animator.SetBool("isInRadius", true);
+            hit();
+        }
     }
 
+    IEnumerator hit()
+    {
+        yield return new WaitForSeconds(0.5f);
+        
+    }
     private void OnDrawGizmos()
     {
         
